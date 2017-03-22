ca808fa8dcc26013211dd4e59b806fc7c8883f1a
AlbinJob <albinjob@gmail.com>
2017-03-22 02:25:00 -0400

    hello

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ console.log(`Server listening at port: ${port}`);
 
 app.use('/hello', (req, res, next) => {
   res.setHeader('Content-Type','text/plain');
-  res.end("hello 1!")
+  res.end("hello !")
   next();
 });
 
