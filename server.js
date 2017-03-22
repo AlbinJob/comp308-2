ceab7560eadab62103da65602b5702eef3e5d267
AlbinJob <albinjob@gmail.com>
2017-03-22 01:07:17 -0400

    router

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 // our sever is connect server
+
+let connect = require('connect');
+let http = require('http');
+ 
+let app = connect();
+const port=3000;
+
+app.listen(port);
+console.log(`Server listening at port: ${port}`);
+
+app.use('/hello', (req, res, next) => {
+  res.setHeader('Content-Type','text/plain');
+  res.end("hello!")
+  next();
+});
+
+app.use('/', (req, res, next) => {
+  res.setHeader('Content-Type','text/plain');
+  res.end("Welcome!")
+  next();
+});
+
